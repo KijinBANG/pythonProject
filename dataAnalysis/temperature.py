da7fa5f7f37dff0654a53e9ea49d5015fa784d1a
KijinBANG <mr.kijin.bang@gmail.com>
2022-06-28 17:42:36 +0900

    let's study about 'dataAnalysis'

--- dataAnalysis/temperature.py
+++ dataAnalysis/temperature.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 f = open('ta_20220628155123.csv', 'r', encoding='cp949')
 data = csv.reader(f, delimiter=',')
@@ -6,7 +7,7 @@ header = next(data)
 print(header)
 
 lst = []
-tempDate, temp, m = "", 0.0, 0.0
+tempDateLow, tempDateHigh, temp, m, M = "", "", 0.0, 0.0, 0.0
 for row in data:
     for idx in range(2, 5):
         try:
@@ -16,11 +17,21 @@ for row in data:
             lst.append(row)
         row[idx] = temp
     # print(row)
-    print("??", m, row[3], m > row[3])
+    # print("??", m, row[3], m > row[3])
     if m > row[3]:
-        tempDate = row[0]
+        tempDateLow = row[0]
         m = row[3]
-    print("test", tempDate, m)
+    if M < row[4]:
+        tempDateHigh = row[0]
+        M = row[4]
+    # print("test", tempDateLow, m)
 # print(lst)
-print(tempDate, m)
+print("가장 추웠던 날", tempDateLow, m)
+print("가장 더웠던 날", tempDateHigh, M)
 f.close()
+
+plt.title("histogram")
+plt.plot([12, 43, 25, 15], label='first')
+plt.plot([10, 30, 20, 40], label='second')
+plt.legend()
+plt.show()
